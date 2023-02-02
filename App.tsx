8ce86f65609c3bd3eea28f1783115d4c87679f30
Blu3Orang3 <matealtunashvili@gmail.com>
2023-02-03 01:05:06 +0400

    initial project setup

---
+++ App.tsx
@@ -0,0 +1,17 @@
+import {Text, View} from 'react-native';
+import colors from './src/theme/colors';
+import font from './src/theme/fonts';
+import AntDesign from 'react-native-vector-icons/AntDesign';
+
+const App = () => {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text style={{color: colors.primary, fontSize: font.size.xlg}}>
+        Hello Native React!
+        <AntDesign name="stepforward" size={24} />
+      </Text>
+    </View>
+  );
+};
+
+export default App;

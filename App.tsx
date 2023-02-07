1f0aff5c33ab3cdef45295cda62b6bf1371a09b0
Blu3Orang3 <matealtunashvili@gmail.com>
2023-02-08 02:53:25 +0400

    post static component design finished

--- App.tsx
+++ App.tsx
@@ -1,17 +1,145 @@
-import {Text, View} from 'react-native';
+import {Text, View, Image, StyleSheet} from 'react-native';
 import colors from './src/theme/colors';
-import font from './src/theme/fonts';
+import fonts from './src/theme/fonts';
 import AntDesign from 'react-native-vector-icons/AntDesign';
+import Entypo from 'react-native-vector-icons/Entypo';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import Feather from 'react-native-vector-icons/Feather';
 
 const App = () => {
   return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text style={{color: colors.primary, fontSize: font.size.xlg}}>
-        Hello Native React!
-        <AntDesign name="stepforward" size={24} />
-      </Text>
+    <View style={styles.post}>
+      {/* Header */}
+      <View style={styles.header}>
+        <Image
+          source={{
+            uri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/1.jpg',
+          }}
+          style={styles.userAvatar}
+        />
+        <Text style={styles.userName}>vadimnotjustdev</Text>
+
+        <Entypo
+          name="dots-three-horizontal"
+          size={16}
+          style={styles.threeDots}
+        />
+      </View>
+
+      {/* Content */}
+      <Image
+        source={{
+          uri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/images/1.jpg',
+        }}
+        style={styles.image}
+      />
+
+      {/* Footer */}
+      <View style={styles.footer}>
+        <View style={styles.iconContainer}>
+          <AntDesign
+            name={'hearto'}
+            size={24}
+            style={styles.icon}
+            color={colors.black}
+          />
+          <Ionicons
+            name="chatbubble-outline"
+            size={24}
+            style={styles.icon}
+            color={colors.black}
+          />
+          <Feather
+            name="send"
+            size={24}
+            style={styles.icon}
+            color={colors.black}
+          />
+          <Feather
+            name="bookmark"
+            size={24}
+            style={{marginLeft: 'auto'}}
+            color={colors.black}
+          />
+        </View>
+        {/* likes */}
+        <Text style={styles.text}>
+          Liked by <Text style={styles.bold}>xxtent</Text>{' '}
+          <Text style={styles.bold}>and 66 others</Text>
+        </Text>
+        {/* post description */}
+        <Text style={styles.text}>
+          <Text style={styles.bold}>vadimnotjustdev</Text> {/*  */}
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima quos
+          impedit sapiente delectus eum optio.
+        </Text>
+        {/* comments */}
+        <Text>View all 16 comments</Text>
+        <View style={styles.comment}>
+          <Text style={styles.commentText}>
+            <Text style={styles.bold}>vadimnotjustdev</Text> {/*  */}
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          </Text>
+          <AntDesign name={'hearto'} style={styles.icon} color={colors.black} />
+        </View>
+
+        {/* Post Date*/}
+        <Text>19 December, 2023</Text>
+      </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  post: {},
+  image: {
+    width: '100%',
+    aspectRatio: 1,
+  },
+  header: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    padding: 10,
+  },
+  userAvatar: {
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+    marginRight: 10,
+  },
+  userName: {
+    fontWeight: fonts.weight.bold,
+    color: colors.black,
+  },
+  threeDots: {
+    marginLeft: 'auto',
+  },
+  footer: {
+    padding: 10,
+  },
+  iconContainer: {
+    flexDirection: 'row',
+    marginBottom: 5,
+  },
+  icon: {
+    marginHorizontal: 5,
+  },
+  text: {
+    color: colors.black,
+    lineHeight: 18,
+  },
+  commentText: {
+    flex: 1,
+    color: colors.black,
+    lineHeight: 18,
+  },
+  bold: {
+    fontWeight: fonts.weight.bold,
+  },
+  comment: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+});
 
 export default App;
